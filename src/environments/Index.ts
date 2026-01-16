82f249a23a9710bece9cf8e144d5b618e23bba16
Felipe Nogueira Pereira <felipenogueirap7@gmail.com>
2026-01-16 18:23:55 -0300

    adjusment environment

--- src/environments/Index.ts
+++ src/environments/Index.ts
@@ -1,5 +1,6 @@
 export const Environment = {
-    BASE_URL: 'http://localhost:8080/v1',
+    //BASE_URL: 'http://localhost:8080/v1',
+    BASE_URL: 'https://cantinaibjapi-production.up.railway.app/v1',
     MAIN_COLOR: '#d04d27',
     //MAIN_COLOR: '#d62828',
     //MAIN_COLOR: '#0668E1',
